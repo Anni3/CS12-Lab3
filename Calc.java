924b182b29bff75224b7a2b9db14f41ca62b41f1
Anni3 <annievolovik@gmail.com>
2014-10-26 10:36:32 -0700

    HW
    Working on depth()

--- Calc.java
+++ Calc.java
@@ -12,26 +12,50 @@ public class Calc {
 	}
 	
 	//Push a number
-	public void push(double x){}
+	public void push(double x){
+		top++;
+		numStack[top]=var;
+	}
 	
 	//Pop top number (removes)
 	public double pop(){
-		return 0.0;
+		if (top==-1) throw new RuntimeException();
+		top--;
+		int incremTop = top+1;
+		return numStack[incremTop];
 	}
 	
 	//Peek at top number (don't remove)
 	public double peek(){
-		return 0.0;
+		if (top==-1) throw new RuntimeExpection();
+		return numStack[top];
 	}
 	
 	//Add top two numbers
-	public void add(){}
+	public void add(){
+		if(top<1) throw new RuntimeExpection();
+		//addition: 1st = 1st + 2nd
+		numStack[top-1] = numStack[top-1] + numStack[top];
+		top--;
+	}
 	
 	//Subtract top two numbers (top on right side)
-	public void subtract(){}
+	public void subtract(){
+		if (top<1) throw new RuntimeExpection();
+		// 1st = 1st - 2nd
+		numStack[top-1] = numStack[top-1]-numStack[top];
+		top--;
+	}
 	
 	//multiply top two numbers
-	public void multiple(){}
+	public void multiple(){
+		if(top<1) throw new RuntimeExpection();
+	}
+	
+	//divide top two numbers
+	public void divide(){
+		if(top<1) throw new RuntimeExpection();
+	}
 	
 	//return how many numbers are in the stack
 	public int depth(){
